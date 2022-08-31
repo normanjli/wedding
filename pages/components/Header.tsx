5148fff322898d8efb47d3db7b8f2e13c54ab2be
normanjli <normanjli@gmail.com>
2022-08-30 20:02:31 -0600

    initial

---
+++ pages/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./Header.styles";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <p>test</p>
+    </StyledHeader>
+  );
+};
+
+export default Header;
