2bcf5e1db56940b6ccde803d66d7f1dd7fe8abc9
Josh Ice <josh.j.ice@gmail.com>
2022-10-24 12:50:20 -0700

    firebase connection, folder structure changes, test endpoint

---
+++ src/utils/firebase.ts
@@ -0,0 +1,10 @@
+import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { getFirestore } from 'firebase-admin/firestore';
+
+const firebaseConfig = {
+   credential: applicationDefault(),
+   databaseURL: 'https://wedding-3d481.firebaseio.com'
+}
+
+export const firebase = initializeApp(firebaseConfig);
+export const db = getFirestore(firebase);
