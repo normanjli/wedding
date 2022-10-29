d3c25ebd0c968ac901324b1cadcb85c6eb48f0f7
normanjli <normanjli@gmail.com>
2022-10-29 15:55:10 -0600

    feat: addes page-layout

--- src/components/Header.styles.tsx
+++ src/components/Header.styles.tsx
@@ -2,10 +2,11 @@ import styled, { StyledComponent } from 'styled-components';
 import { Colors } from '../Styles/Colors';
 
 export const StyledHeader = styled.header`
-  display: flex;
+  grid-template-rows: 1fr 1fr;
+  display: grid;
   width: 100%;
-  justify-content: space-evenly;
-  align-items: center;
+  align-content: center;
+  text-align: center;
 `;
 interface HeaderLinkProps {
   active: boolean;
@@ -16,10 +17,37 @@ export const HeaderLink = styled.a<HeaderLinkProps>`
   text-decoration: ${(props) => (props.active ? 'underline' : 'unset')};
   font-family: 'Roboto', sans-serif;
   color: ${Colors.primary};
+  font-size: 18px;
+  width: 'fit-content';
+  :hover {
+    text-decoration: underline;
+  }
 `;
 
-export const ActiveHeaderLink = styled(HeaderLink)`
-  font-size: 16px;
-  font-weight: 600;
-  text-decoration: underline;
+export const Nav = styled.nav`
+  display: flex;
+  width: 100%;
+  max-width: 700px;
+  justify-content: space-evenly;
+  text-align: center;
+  margin: auto;
+`;
+
+export const HeaderTitle = styled.h1`
+  margin: 0;
+  margin-top: 20px;
+  font-family: 'Roboto', sans-serif;
+  text-align: center;
+  color: ${Colors.primary};
+  font-size: 40px;
+`;
+
+export const HeaderSubTitle = styled.h4`
+  font-family: 'Roboto', sans-serif;
+  text-align: center;
+  color: ${Colors.primary};
+  font-size: 24px;
+  margin: 0;
+  margin-top: 10px;
+  font-weight: 400;
 `;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { GlobalStyle } from '../components/GlobalStyle.styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Kaitlin and Norman&apos;s Wedding</title>
       </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
