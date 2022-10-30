59322bd589069213079b85d07384625a8f728fd1
normanjli <normanjli@gmail.com>
2022-10-29 19:49:26 -0600

    fix: naming

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -28,9 +28,9 @@ const Header = () => {
               Where to stay
             </HeaderLink>
           </Link>
-          <Link href="/schedule" passHref>
-            <HeaderLink active={router.pathname === '/schedule'}>
-              Schedule
+          <Link href="/eventinfo" passHref>
+            <HeaderLink active={router.pathname === '/eventinfo'}>
+              Event Information
             </HeaderLink>
           </Link>
           <Link href="/registry" passHref>
